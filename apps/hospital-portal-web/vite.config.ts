536a45800f9b0a600b6367165debcd72fade524c
Yamlak Negash <yamlaknegash96@gmail.com>
2026-06-16 02:54:06 +0300

    fix : hospital and donor vercel issues fixed.

--- apps/hospital-portal-web/vite.config.ts
+++ apps/hospital-portal-web/vite.config.ts
@@ -1,12 +1,10 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
